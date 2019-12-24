d38ce1c7fb11d57c1dd775d36b695e353eed8179
ambiesoft <ambiesoft.trueff@gmail.com>
2019-12-24 15:31:26 +0900

    start

---
+++ slnverchecker/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace slnverchecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var productName = Assembly.GetEntryAssembly()
+                .GetCustomAttributes(typeof(AssemblyProductAttribute))
+                .OfType<AssemblyProductAttribute>()
+                .FirstOrDefault().Product;
+
+            this.Title = productName;
+        }
+
+        SolutionVersionInfo svi_;
+
+        private void btnBrowseSolution_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new Microsoft.Win32.OpenFileDialog() {
+                Filter = "Solution Files (*.sln)|*.sln"
+            };
+            var result = ofd.ShowDialog();
+            if (result == false)
+                return;
+            txtSolutionPath.Text = ofd.FileName;
+
+            List<string> lines = new List<string>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(txtSolutionPath.Text))
+                {
+                    string line;
+                    for (; (line = sr.ReadLine()) != null;)
+                    {
+                        lines.Add(line);
+                    }
+
+                    svi_ = new SolutionVersionInfo(lines.ToArray());
+                    tbSolutionVersion.Text = svi_.ToString();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                tbSolutionVersion.Text = ex.Message;
+                return;
+            }
+        }
+    }
+}
---
+++ slnverchecker/SolutionVersionInfo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slnverchecker
+{
+    class SolutionVersionInfo
+    {
+        string format_;
+        string comment_;
+        string vsversion_;
+        string minimumvsversion_;
+        public SolutionVersionInfo(string[] lines)
+        {
+            foreach(string lineorig in lines)
+            {
+                string line = lineorig.Trim();
+                if (line.Length == 0)
+                    continue;
+
+                if(format_==null && line.StartsWith("Microsoft Visual Studio Solution File, Format Version "))
+                {
+                    format_ = line;
+                    continue;
+                }
+                if(comment_==null && line.StartsWith("# Visual Studio "))
+                {
+                    comment_ = line;
+                    continue;
+                }
+                if(vsversion_==null && line.StartsWith("VisualStudioVersion = "))
+                {
+                    vsversion_ = line;
+                    continue;
+                }
+                if (minimumvsversion_ == null && line.StartsWith("MinimumVisualStudioVersion = "))
+                {
+                    minimumvsversion_ = line;
+                    continue;
+                }
+                break;
+            }
+        }
+
+        public override string ToString() 
+        {
+            if(string.IsNullOrEmpty(format_) ||
+                string.IsNullOrEmpty(comment_) ||
+                string.IsNullOrEmpty(vsversion_) ||
+                string.IsNullOrEmpty(minimumvsversion_))
+            {
+                return "ILLEGAL SLN";
+            }
+            var sb = new StringBuilder();
+            sb.AppendLine();
+            sb.AppendLine(format_);
+            sb.AppendLine(comment_);
+            sb.AppendLine(vsversion_);
+            sb.AppendLine(minimumvsversion_);
+
+            return sb.ToString();
+        }
+    }
+}
